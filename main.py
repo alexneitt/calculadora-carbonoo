27197f50348c557db48f6ef259b2c174f157d435
alexneitt <alexisrg620@gmail.com>
2025-06-02 02:15:36 -0600

    servidor2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from waitress import serve
-from app import app  # Aquí importas tu objeto Flask que está en app.py
+from app import app
+import os
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    print(f"Iniciando servidor en el puerto {port}")
+    serve(app, host='0.0.0.0', port=port)
